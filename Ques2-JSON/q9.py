c35678cabff53ec1746c4e72db6364564f0b73a9
Soumil-Bhattacharya <47296106+Soumil-Bhattacharya@users.noreply.github.com>
2023-05-21 11:42:17 +0530

    additional files

---
+++ Ques2-JSON/q9.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+gender_counts = {
+    "Male": 20,
+    "Female": 15,
+    "Others": 5
+}
+
+plt.pie(gender_counts.values(), labels=gender_counts.keys(), autopct='%1.1f%%')
+
+plt.title("Appointments by Gender")
+
+plt.show()
+
